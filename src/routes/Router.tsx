aa8c993a277c7259caf919b2bd9a7fc2d2d69380
dabi_vm <dvmblood07@gmail.com>
2022-08-10 12:49:51 +0430

    #1: routes

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,5 +1,9 @@
 import { Home } from "../components/Home/Home";
+import { Settings } from "../components/Settings/Settings";
 
-const MainRoutes = [{ path: "/", component: <Home /> }];
+const MainRoutes = [
+  { path: "/", component: <Home /> },
+  { path: "/profile/settings", component: <Settings /> },
+];
 
 export default MainRoutes;
