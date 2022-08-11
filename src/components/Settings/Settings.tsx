5427a437e1d8f7efaa7e9be713f9e4982dfd4dc6
dabi_vm <dvmblood07@gmail.com>
2022-08-11 10:51:07 +0430

    #2: duplicate data checking

--- src/components/Settings/Settings.tsx
+++ src/components/Settings/Settings.tsx
@@ -3,8 +3,9 @@ import { Navbar } from "../Navbar/Navbar";
 import { CustomPaper } from "./SettingsStyles";
 import { LinksList } from "./LinksList/LinksList";
 import { AddLink } from "./AddLink/AddLink";
-import { useState } from "react";
+import { useContext, useState } from "react";
 import { ILinkItem } from "../../models/links";
+import { SnackContext } from "../../context/SnackContext";
 
 const breadcrumbs = [
   {
@@ -23,9 +24,24 @@ const breadcrumbs = [
 
 export const Settings = () => {
   const [linksList, setLinksList] = useState<ILinkItem[]>([]);
+  const { showAlert } = useContext(SnackContext);
+
+  const DuplicateCheck = (v: ILinkItem) => {
+    const isDuplicate = linksList.some(
+      (item) => item.link === v.link && item.title === v.title
+    );
+    return isDuplicate;
+  };
 
   const HandleAddLink = (v: ILinkItem) => {
-    setLinksList((preState) => [...preState, v]);
+    if (DuplicateCheck(v)) {
+      showAlert({
+        type: "warning",
+        message: "مقادیر وارد شده تکراری است",
+      });
+    } else {
+      setLinksList((preState) => [...preState, v]);
+    }
   };
 
   const HandleDeleteLink = (id: string) => {
@@ -33,22 +49,29 @@ export const Settings = () => {
   };
 
   const HandleEditLink = (v: ILinkItem) => {
-    // find item and replace new item data
-    setLinksList((preState) =>
-      preState.map((el) => {
-        if (el.id === v.id) {
-          el.link = v.link;
-          el.title = v.title;
-        }
-        return el;
-      })
-    );
+    if (DuplicateCheck(v)) {
+      showAlert({
+        type: "warning",
+        message: "مقادیر وارد شده تکراری است",
+      });
+    } else {
+      // find item and replace new item data
+      setLinksList((preState) =>
+        preState.map((el) => {
+          if (el.id === v.id) {
+            el.link = v.link;
+            el.title = v.title;
+          }
+          return el;
+        })
+      );
+    }
   };
 
   return (
     <Grid container>
       <Navbar title="تنظیمات کاربری" breadcrumbs={breadcrumbs} />
-      <Grid item xs={12} >
+      <Grid item xs={12}>
         <CustomPaper variant="black90" elevation={1}>
           <p>مسیرهای ارتباطی</p>
           <AddLink handleSubmit={HandleAddLink} />
