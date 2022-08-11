67bf002184ef727f1b2549a6bf69de25dc7c87ad
dabi_vm <dvmblood07@gmail.com>
2022-08-11 13:56:31 +0430

    #2: minor fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,25 @@
+import { ContextProviders } from "./context/ContextProviders";
 import ModalContextProvider from "./context/ModalContext";
+import MultiLanguageContext from "./context/MultiLanguageContext";
 import SnackContextProvider from "./context/SnackContext";
 import ThemeContextProvider from "./context/ThemeContext";
 import { MainLayout } from "./layouts/MainLayout";
 import ThemeProvider from "./themes/ThemeProvider";
 
 function App() {
+
   return (
-    <ThemeContextProvider>
-      <ThemeProvider>
-        <SnackContextProvider>
-          <ModalContextProvider>
-            <MainLayout />
-          </ModalContextProvider>
-        </SnackContextProvider>
-      </ThemeProvider>
-    </ThemeContextProvider>
+    <ContextProviders
+      contextProviders={[
+        ModalContextProvider,
+        SnackContextProvider,
+        ThemeContextProvider,
+        ThemeProvider,
+        MultiLanguageContext,
+      ]}
+    >
+        <MainLayout />
+    </ContextProviders>
   );
 }
 
