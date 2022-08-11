e576d2b459615210bee4e645525fe5d760f7d92b
dabi_vm <dvmblood07@gmail.com>
2022-08-11 13:54:38 +0430

    #2: change dir in theme

--- src/themes/ThemeProvider.tsx
+++ src/themes/ThemeProvider.tsx
@@ -2,14 +2,17 @@ import { FC, useContext } from "react";
 import { ThemeProvider as MuiThemeProvider } from "@mui/material";
 import { getThemeByName } from "./base";
 import { ThemeContext } from "../context/ThemeContext";
+import { MultiLanguageContext } from "../context/MultiLanguageContext";
 
 interface IProps {
   children: any;
 }
 const ThemeProvider: FC<IProps> = ({ children }) => {
   const { dark } = useContext(ThemeContext);
+  const { multiLang } = useContext(MultiLanguageContext);
   const theme = getThemeByName(dark ? "darkTheme" : "lightTheme");
-
+  theme.direction = multiLang.isRTl ? "rtl" : "ltr";
+  
   return <MuiThemeProvider theme={theme}>{children}</MuiThemeProvider>;
 };
 
